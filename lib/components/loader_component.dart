c88ff0690d20b6eb768e0bf21ebe718f26a5434b
Luis Núñez <luisalbertonu@gmail.com>
2025-09-29 12:24:08 -0300

    Cambio

--- lib/components/loader_component.dart
+++ lib/components/loader_component.dart
@@ -4,7 +4,7 @@ class LoaderComponent extends StatelessWidget {
   final String text;
 
   const LoaderComponent({Key? key, this.text = ''}) : super(key: key);
-
+  //--
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -12,20 +12,17 @@ class LoaderComponent extends StatelessWidget {
         width: 200,
         height: 100,
         decoration: BoxDecoration(
-            color: const Color(0xFFFFFFFF),
-            borderRadius: BorderRadius.circular(10),
-            border: Border.all(
-              color: const Color.fromARGB(255, 24, 207, 36),
-            )),
+          color: const Color(0xFFFFFFFF),
+          borderRadius: BorderRadius.circular(10),
+          border: Border.all(color: const Color.fromARGB(255, 24, 207, 36)),
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             const CircularProgressIndicator(
               color: Color.fromARGB(255, 24, 207, 36),
             ),
-            const SizedBox(
-              height: 20,
-            ),
+            const SizedBox(height: 20),
             Text(text, style: const TextStyle(fontSize: 15)),
           ],
         ),
